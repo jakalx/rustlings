fe534a63a2fdac85fae3eb8d6943892344100c8c
Alexander Kobjolke <me@failco.de>
2023-02-04 22:57:38 +0100

    Solve options3

--- exercises/options/options3.rs
+++ exercises/options/options3.rs
@@ -13,7 +13,7 @@ fn main() {
 
     // Fix without changing the following line
     match y {
-        Some(p) => println!("Co-ordinates are {},{} ", p.x, p.y),
+        Some(ref p) => println!("Co-ordinates are {},{} ", p.x, p.y),
         _ => println!("no match"),
     }
     y; // Fix without deleting this line.
