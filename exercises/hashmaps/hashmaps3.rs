a14814d1a1283656db68a68b49224275511680b2
Alexander Kobjolke <me@failco.de>
2023-02-04 22:52:14 +0100

    Solve exercises

--- exercises/hashmaps/hashmaps3.rs
+++ exercises/hashmaps/hashmaps3.rs
@@ -14,8 +14,6 @@
 
 // Execute `rustlings hint hashmaps3` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
-
 use std::collections::HashMap;
 
 // A structure to store team name and its goal details.
@@ -23,6 +21,21 @@ struct Team {
     name: String,
     goals_scored: u8,
     goals_conceded: u8,
+}
+
+impl Team {
+    fn new(name: String, goals_scored: u8, goals_conceded: u8) -> Team {
+        Team {
+            name,
+            goals_scored,
+            goals_conceded,
+        }
+    }
+
+    fn update(&mut self, team: &Team) {
+        self.goals_scored += team.goals_scored;
+        self.goals_conceded += team.goals_conceded;
+    }
 }
 
 fn build_scores_table(results: String) -> HashMap<String, Team> {
@@ -35,11 +48,19 @@ fn build_scores_table(results: String) -> HashMap<String, Team> {
         let team_1_score: u8 = v[2].parse().unwrap();
         let team_2_name = v[1].to_string();
         let team_2_score: u8 = v[3].parse().unwrap();
-        // TODO: Populate the scores table with details extracted from the
-        // current line. Keep in mind that goals scored by team_1
-        // will be the number of goals conceded from team_2, and similarly
-        // goals scored by team_2 will be the number of goals conceded by
-        // team_1.
+
+        let team_1 = Team::new(team_1_name, team_1_score, team_2_score);
+        let team_2 = Team::new(team_2_name, team_2_score, team_1_score);
+
+        scores
+            .entry(team_1.name.clone())
+            .and_modify(|team| team.update(&team_1))
+            .or_insert(team_1);
+
+        scores
+            .entry(team_2.name.clone())
+            .and_modify(|team| team.update(&team_2))
+            .or_insert(team_2);
     }
     scores
 }
--- exercises/quiz1.rs
+++ exercises/quiz1.rs
@@ -10,10 +10,10 @@
 // Write a function that calculates the price of an order of apples given
 // the quantity bought. No hints this time!
 
-// I AM NOT DONE
-
-// Put your function here!
-// fn calculate_price_of_apples {
+fn calculate_price_of_apples(amount: u32) -> u32 {
+    let price_per_apple = if amount > 40 { 1 } else { 2 };
+    price_per_apple * amount
+}
 
 // Don't modify this function!
 #[test]
