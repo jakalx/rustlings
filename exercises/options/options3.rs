ec4bd4185d0c521cc888f437b8db5fd74a50a576
Alexander Kobjolke <me@failco.de>
2023-02-04 22:53:12 +0100

    options3: Require not to change the scrutinee
    Explanation: The exercise can also be solved by writing:
    
    ```rust
    match &y {
      ...
    }
    ```
    
    which is probably unintended.

--- exercises/options/options3.rs
+++ exercises/options/options3.rs
@@ -11,6 +11,7 @@ struct Point {
 fn main() {
     let y: Option<Point> = Some(Point { x: 100, y: 200 });
 
+    // Fix without changing the following line
     match y {
         Some(p) => println!("Co-ordinates are {},{} ", p.x, p.y),
         _ => println!("no match"),
